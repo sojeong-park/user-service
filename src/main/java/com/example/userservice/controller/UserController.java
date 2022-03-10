55935ce5322fca8c166178068b46dfbf849386ae
박소정 <psj6414@naver.com>
2022-03-10 14:44:15 +0900

    feat: local repository에 저장한 ecommerce.yml(설정파일)읽어오는 기능 구현
    - 변경된 ecommerce.yml을 적용하려면 서버 재기동 필요

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -30,8 +30,10 @@ public class UserController {
 
     @GetMapping("/health-check")
     public String status() {
-        return String.format("It's working in User service ont PORT %s",
-                env.getProperty("local.server.port"));
+        return String.format("It's working in User service"
+                + ", port (local.server.port) =" + env.getProperty("local.server.port")
+                + ", token secret =" + env.getProperty("token.secret")
+                + ", token expiration time =" + env.getProperty("token.expiration_time"));
     }
 
     @GetMapping("/welcome")
